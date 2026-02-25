c69897feec9fadd464c0b48fe3ca5ab4eab4fa4d
Bas Baccarne <bastiaan.baccarne@ugent.be>
2026-02-25 15:35:10 +0100

    2 buttons

--- tests/button/button.py
+++ tests/button/button.py
@@ -2,13 +2,25 @@ from gpiozero import Button
 import time
 
 button_horn = Button(4)
+button_pin3 = Button(3)
+
 debounce = 0.3
 
 while True:
+    # --- Pin 4: trigger on RELEASE ---
     if button_horn.is_pressed:
-        # Wait for release
         while button_horn.is_pressed:
             time.sleep(0.01)
 
-        print("Button released - phone picked up")
+        print("Pin 4 released - phone picked up")
+        time.sleep(debounce)
+
+    # --- Pin 3: trigger on PRESS ---
+    if button_pin3.is_pressed:
+        print("Pin 3 pressed")
+
+        # wait until released to avoid repeat triggers
+        while button_pin3.is_pressed:
+            time.sleep(0.01)
+
         time.sleep(debounce)
