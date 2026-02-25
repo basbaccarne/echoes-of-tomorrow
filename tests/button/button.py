faa38bea41e4e27b2af056d661cbd00922669b7d
Bas Baccarne <bastiaan.baccarne@ugent.be>
2026-02-25 15:31:27 +0100

    button test

--- tests/button/button.py
+++ tests/button/button.py
@@ -1,9 +1,14 @@
 from gpiozero import Button
+import time
+
 button_horn = Button(4)
 debounce = 0.3
-if button_horn.is_pressed:
-            # Wacht op release
-            while button_horn.is_pressed:
-                time.sleep(0.01)
-            print("Button released - phone picked up")
-            time.sleep(debounce)
+
+while True:
+    if button_horn.is_pressed:
+        # Wait for release
+        while button_horn.is_pressed:
+            time.sleep(0.01)
+
+        print("Button released - phone picked up")
+        time.sleep(debounce)
