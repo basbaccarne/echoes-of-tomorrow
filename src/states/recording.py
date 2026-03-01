cb752f032fd32d9336a728838144b7321fd41f62
Bas Baccarne <bastiaan.baccarne@ugent.be>
2026-03-01 07:39:29 +0100

    audio card change

--- src/states/recording.py
+++ src/states/recording.py
@@ -18,7 +18,7 @@ def run():
         print("ALSA message:")
         process = subprocess.Popen([
             "arecord",
-            "-D", "plughw:2,0",
+            "-D", "plughw:1,0",
             "-f", "cd",
             "-t", "wav",
             audio_path
--- src/states/response.py
+++ src/states/response.py
@@ -18,7 +18,7 @@ def run():
     try:
         subprocess.run([
             "aplay",
-            "-D", "plughw:2,0",
+            "-D", "plughw:1,0",
             audio_path
         ], check=True)
     except subprocess.CalledProcessError as e:
