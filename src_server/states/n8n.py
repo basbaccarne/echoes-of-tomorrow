f93072f9d5244d11d085c626b0bbe10c50d13f95
TimTheys9000 <122802815+TimTheys9000@users.noreply.github.com>
2026-03-06 16:39:37 +0100

    Update n8n.py
    Updated with the latest version of Tim his n8n webhook

--- src_server/states/n8n.py
+++ src_server/states/n8n.py
@@ -7,12 +7,12 @@ import yaml
 import os
 
 # WEBHOOKS
-#Tims hook
-#url = "http://localhost:5678/webhook/dae1d29b-5725-47fc-a68b-cba9d669a981/chat"
 #local test
 # url = "http://localhost:5678/webhook/4a373672-3af4-4cae-a776-67fe0c43a3e6/chat" 
 #SSH test
-url = "http://localhost:5678/webhook/70f7a510-eec9-410d-b623-de8bc323273a/chat"
+#url = "http://localhost:5678/webhook/70f7a510-eec9-410d-b623-de8bc323273a/chat"
+#Latest n8n flow webhook
+url = "http://localhost:5678/webhook/ab3e469b-5e4c-4605-b34a-6bde15477a11" #Depending on the phone that is used to call we need to add an URL parameter (e.g. ?future=1) so in n8n we can load the correct future file text (e.g. future1.txt)
 
 TIMEOUT = 60  # wait up to 60s
 
