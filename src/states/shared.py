cdf24b727b702f172f1362db5c9aff113c2c6fd2
Bas Baccarne <bastiaan.baccarne@ugent.be>
2026-03-16 18:42:23 +0100

    path

--- src/states/shared.py
+++ src/states/shared.py
@@ -2,7 +2,7 @@ import os
 import yaml
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIG_PATH = os.path.join(BASE_DIR, "config.yaml")
 
 
